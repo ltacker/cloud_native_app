89d1a87b2128b6f6ddd57585b6a1bccf9dfa5d4b
Lucas Bertrand <lucas.bertrand@ensimag.grenoble-inp.fr>
2018-01-09 17:57:42 +0100

    Add play script test and increased user index from 10 to 100

--- tests/scalability.sh
+++ tests/scalability.sh
@@ -1,6 +1,13 @@
 readonly THRESHOLD=1
 
-timeout $THRESHOLD ./tests/b.sh http://localhost:8082 1 10
+timeout $THRESHOLD ./tests/b.sh http://localhost:8082 1 100
+
+if [ $? -gt 0 ]
+then
+  exit 1
+fi
+
+timeout $THRESHOLD ./tests/play.sh http://localhost:8000/index.html 1 100
 
 if [ $? -gt 0 ]
 then
