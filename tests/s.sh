500ce124358a1ed2dcf3ab922446c97b07ea5768
Paul-Henri Garnier <paul.garnier@fidelise.fr>
2018-01-09 18:04:20 +0100

    test scalabilité s

--- tests/s.sh
+++ tests/s.sh
@@ -1,1 +1,16 @@
-# TODO
+#!/bin/bash
+
+# Generate http requests to check s service scalability
+
+# $1 = url
+# $2 = start id
+# $3 = end id
+# ex : time s.sh http://localhost:8082 1 10
+
+for i in $(seq $2 $3)
+do
+	curl $1/srvb/user/$i &
+done
+
+wait
+exit 0
