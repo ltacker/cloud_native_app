679af4b9967f05fe08911fa7f54cc5c1bdc43ae8
Max tmvt <maxime.tm@gmail.com>
2018-01-25 17:04:38 +0100

    bash test to pytest

---
+++ tests/test_scalability.py
@@ -0,0 +1,45 @@
+from multiprocessing import Pool
+import requests
+import time
+import pytest
+
+PROTOCOL = "http"
+HOSTNAME = "localhost"
+PORT = 80
+NUMBER_OF_ID = 100
+CONNECTION_TIMEOUT = 10
+TEST_TIMEOUT = 10
+
+def url_list(service, ids):
+    for i in ids:
+        yield PROTOCOL + "://" + HOSTNAME + ":" + str(PORT) + "/srv" + service + "/usr/" + i
+
+def req(url):
+    r = requests.get(url, timeout=CONNECTION_TIMEOUT)
+    r.raise_for_status()
+    return r
+
+def pool_request(service, ids):
+    p = Pool()
+    p.map_async(req, url_list(service, ids))
+    p.wait()
+    return p
+
+def pool_time(service):
+    start = time.time()
+    p = pool_request(service, range(1, NUMBER_OF_ID + 1))
+    if not p.successful():
+        return None
+    return time.time() - start
+
+@pytest.mark.parametrize("service,timeout", [
+    ("b", TEST_TIMEOUT),
+    ("i", TEST_TIMEOUT),
+    ("p", TEST_TIMEOUT),
+    ("s", TEST_TIMEOUT),
+    ("w", TEST_TIMEOUT)
+])
+def test_services(service, timeout):
+    t = pool_time(service)
+    assert t is not None
+    assert t < timeout
