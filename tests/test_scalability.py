1fb8546c6a2b063def0ac35eb61087220704e1a5
Max tmvt <maxime.tm@gmail.com>
2018-01-26 13:35:25 +0100

    fix test handle different path per services

--- tests/test_scalability.py
+++ tests/test_scalability.py
@@ -10,32 +10,32 @@ NUMBER_OF_ID = 9
 CONNECTION_TIMEOUT = 10
 TEST_TIMEOUT = 10
 
-def url_list(service, ids):
+def url_list(service, path, ids):
     for i in ids:
-        yield PROTOCOL + "://" + HOSTNAME + ":" + str(PORT) + "/srv" + service + "/user/" + str(i)
+        yield PROTOCOL + "://" + HOSTNAME + ":" + str(PORT) + "/srv" + service + path + "/" + str(i)
 
 def req(url):
     r = requests.get(url, timeout=CONNECTION_TIMEOUT)
     r.raise_for_status()
     return r
 
-def pool_request(service, ids):
+def pool_request(service, path, ids):
     p = Pool()
-    return p.map(req, url_list(service, ids))
+    return p.map(req, url_list(service, path, ids))
 
-def pool_time(service):
+def pool_time(service, path):
     start = time.time()
-    p = pool_request(service, range(1, NUMBER_OF_ID + 1))
+    p = pool_request(service, path, range(1, NUMBER_OF_ID + 1))
     return time.time() - start
 
-@pytest.mark.parametrize("service,timeout", [
-    ("b", TEST_TIMEOUT),
-    ("i", TEST_TIMEOUT),
-    ("p", TEST_TIMEOUT),
-    ("s", TEST_TIMEOUT),
-    ("w", TEST_TIMEOUT)
+@pytest.mark.parametrize("service,path,timeout", [
+    ("b", "/user", TEST_TIMEOUT),
+    ("i", "/user", TEST_TIMEOUT),
+    ("p", "/user", TEST_TIMEOUT),
+    ("s", "/user", TEST_TIMEOUT),
+    ("w", "/play", TEST_TIMEOUT)
 ])
-def test_services(service, timeout):
-    t = pool_time(service)
+def test_services(service, path, timeout):
+    t = pool_time(service, path)
     assert t is not None
     assert t < timeout
